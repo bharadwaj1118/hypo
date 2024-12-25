974c7e69f9c79ee9563e15d96575de9fbcde05be
bharadwaj1118 <bharadwaj.kanukoti@gmail.com>
2024-12-24 19:02:27 -0600

    the basic setup

---
+++ components/hello.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Hello = () => {
+  return (
+    <div className="text-primary-500">Hello world, I am from the server!</div>
+  );
+};
+
+export default Hello;
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,56 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "standard",
+    "plugin:tailwindcss/recommended",
+    "prettier"
+  ),
+  {
+    plugins: {
+      // eslint-disable-next-line @typescript-eslint/no-require-imports
+      import: require("eslint-plugin-import"),
+    },
+    rules: {
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            ["parent", "sibling"],
+            "index",
+            "object",
+          ],
+          "newlines-between": "always",
+          pathGroups: [
+            {
+              pattern: "@app/**",
+              group: "external",
+              position: "after",
+            },
+          ],
+          pathGroupsExcludedImportTypes: ["builtin"],
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
+    ignores: ["components/ui/**"],
+    overrides: [
+      {
+        files: ["*.ts", "*.tsx"],
+        rules: {
+          "no-undef": "off",
+        },
+      },
+    ],
+  },
 ];
 
 export default eslintConfig;
